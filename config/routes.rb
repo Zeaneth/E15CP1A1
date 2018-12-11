4127e417b491ba226197fc93c1d7d842313da539
Zamiz <zamiztobar@gmail.com>
2018-12-11 20:35:30 -0300

    +sessions routes +empty sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :histories
+  resources :sessions, only: [:create, :destroy]
 
   get 'users/sign_up', to: "users#new"
   post 'users', to: "users#create"
