d3d4dcc2abb7c1a8dc89c1cfdff0cf5e4704bb00
Zamiz <zamiztobar@gmail.com>
2018-12-11 21:25:57 -0300

    +sessions controller methods create, destroy, new +navbar links to users_logs +session#new to sign_in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,20 @@
 class SessionsController < ApplicationController
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.find_by(email: params[:user][:email]).try(:authenticate, params[:user][:password])
+        if @user.present?
+            session[:user_id] = @user.id
+            redirect_to root_path
+        else
+            redirect_to users_sign_in_path
+        end
+    end
+
+    def destroy
+        reset_session
+        redirect_to root_path
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
 
   get 'users/sign_up', to: "users#new"
   post 'users', to: "users#create"
+  get 'users/sign_in', to: 'sessions#new'
+
   root "histories#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
