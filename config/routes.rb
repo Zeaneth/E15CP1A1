c7c24aa7814d2a576f9dae7bc4c051831373680e
Zamiz <zamiztobar@gmail.com>
2018-12-11 21:30:20 -0300

    +sign in route

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   post 'users', to: "users#create"
   get 'users/sign_in', to: 'sessions#new'
 
+
   root "histories#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
